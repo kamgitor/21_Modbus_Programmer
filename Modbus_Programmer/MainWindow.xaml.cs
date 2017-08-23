f2d59df3bd98e0c9c9d671a129975cccfb54b35b
Kamil <kamil@altel.pl>
2017-08-23 12:48:04 +0200

    Pierwszy Comit. Dziala wybor portu, connect, wysylanie i odbieranie - trzeba zrobic ramkowanie odbioru

---
+++ Modbus_Programmer/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Modbus_Programmer
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		SerialPort KamSerial;
+
+		// ***************************************************************
+		public Window1()
+		{
+			InitializeComponent();
+
+			AppInit();
+		
+		}	// Window1
+
+
+		// ***************************************************************
+		public void AppInit()
+		{
+			KamSerial = new SerialPort(comboBoxPorts, rx_function);
+
+			
+
+		}	// AppInit
+
+
+		// ***************************************************************
+		private void button1_Click(object sender, RoutedEventArgs e)
+		{
+			byte[] buf = {1, 2, 3, 4, 5};
+
+			KamSerial.PreparePort();
+
+			KamSerial.BaudRate = 19200;
+			bool isopen = KamSerial.IsOpen;
+			KamSerial.Write(buf, 0, 5);
+
+		}	// AppInit
+
+
+		// ***************************************************************************
+		void rx_function(byte [] buf, int size)
+		{
+			// throw new NotImplementedException();
+
+		}	// SerialPort
+
+	}
+
+}
---
+++ Modbus_Programmer/SerialPort.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Controls;
+
+
+
+namespace Modbus_Programmer
+{
+
+	delegate void rx_ext_funct(byte[] buf, int size);
+
+	class SerialPort : System.IO.Ports.SerialPort
+	{
+		private ComboBox combobox;
+
+		private rx_ext_funct rx_funct;
+
+		// ***************************************************************************
+		public SerialPort(ComboBox combo, rx_ext_funct funct)
+		{
+			combobox = combo;
+			InitSerialComboBox();
+
+			rx_funct = funct;
+
+			this.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(rx_handler_funct);
+
+
+		}	// SerialPort
+
+
+
+		// ***************************************************************************
+		void rx_handler_funct(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+		{
+
+			int rx_size = this.BytesToRead;
+			byte[] buffer = new byte[rx_size];
+
+			int iReaded = this.Read(buffer, 0, rx_size);
+
+			rx_funct(buffer, rx_size);
+
+		}	// rx_handler_funct
+
+
+
+		// ***************************************************************************
+		// Wsadzenie istniejacych portow do comboboxa
+		private void InitSerialComboBox()
+		{
+			string [] portNames = System.IO.Ports.SerialPort.GetPortNames();
+
+			combobox.Items.Clear();
+
+			for (int i = 0; i < portNames.Length; i++)
+			{
+				combobox.Items.Add(portNames[i]);
+			}
+
+		}	// InitSerialPorts
+
+
+		// ***************************************************************************
+		// Przygotowuje port do odpalenia ret false - problem
+		public bool PreparePort()
+		{
+			if (this.IsOpen)
+				this.Close();
+
+			if (combobox.SelectedIndex != -1)
+			{
+				this.PortName = (string)combobox.Items[combobox.SelectedIndex];
+				this.Open();
+
+				return true;
+			}
+			else
+				return false;		// not selected
+
+		}	// GetComboboxPos
+
+
+		/*
+
+		Wojtek uzywal
+		IsOpen
+		Close
+		Write(buffer, 0, length);
+		 
+			int iReadedBytes = serialPort1.BytesToRead;
+		int iReaded = serialPort1.Read(buffer, 0, iReadedBytes);
+		
+		BaudRate = 19200;
+		DataReceived
+		 
+		 
+		 */
+
+	}
+}
