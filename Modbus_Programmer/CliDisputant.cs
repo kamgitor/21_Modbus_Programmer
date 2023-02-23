51c2b23ae873bb058e426b000ef66917a11989ca
Kamil Nowak <kamil.nowak@diasemi.com>
2023-02-23 07:39:46 +0100

    New branch CLI serial disputant

---
+++ Modbus_Programmer/CliDisputant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+
+namespace Modbus_Programmer
+{
+    class CliDisputant : SerialPortGeneric
+    {
+
+        // Konstruktor podobno nie jest dziedziczony - taka konstrukcja zeby byl dziwdziczony
+        public CliDisputant(ComboBox combo) : base(combo)
+        {
+
+        }	// Rs485
+
+
+    }
+}
